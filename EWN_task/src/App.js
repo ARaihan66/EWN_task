7fec7255158eb9f995a4a136446463ea208d14d0
Abu Raihan <aburaihancse066@gmail.com>
2022-12-10 02:34:07 +0600

    Fetch data done

--- EWN_task/src/App.js
+++ EWN_task/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = () => {
+    axios("https://jsonplaceholder.typicode.com/comments")
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data)
+      }).catch((err) => {
+        setError(err.message)
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
