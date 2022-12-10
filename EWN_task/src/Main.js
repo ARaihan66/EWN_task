47ca835c7ab43da9d02eeeb0321f8f6a15f494fd
Abu Raihan <aburaihancse066@gmail.com>
2022-12-10 06:13:03 +0600

    Add column header

--- EWN_task/src/Main.js
+++ EWN_task/src/Main.js
@@ -2,8 +2,25 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Main = () => {
-    const [data, setData] = useState([])
-    const [error, setError] = useState('')
+    const [data, setData] = useState([]);
+    const [error, setError] = useState('');
+
+    const columns = [{
+        dataField: "email",
+        text: "Email",
+        sort: true
+    },
+    {
+        dataField: "postId",
+        text: "ID",
+        sort: true
+    },
+    {
+        dataField: "name",
+        text: "Name",
+        sort: true
+    }
+    ]
 
     useEffect(() => {
         getData();
@@ -12,12 +29,13 @@ const Main = () => {
     const getData = () => {
         axios("https://jsonplaceholder.typicode.com/comments")
             .then((res) => {
-                setData(res.json);
+                setData(res.data);
                 console.log(res.data)
             }).catch((err) => {
                 setError(err.message)
             })
     }
+
 
     return (
         <div>
